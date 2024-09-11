087846a78c50afe7ad298c3e06d399fd609d73af
Max Pobedinsky <95541283+Pobedinsky@users.noreply.github.com>
2024-09-11 08:41:51 +0100

    Create FunctionTester.java

---
+++ FunctionTester.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionTester<T, R> {
+    private List<T> inputs;
+    private List<R> expectedResults;
+    private Function<T, R> functionToTest;
+
+    public FunctionTester(List<T> inputs, List<R> expectedResults, Function<T, R> functionToTest) {
+        this.inputs = inputs;
+        this.expectedResults = expectedResults;
+        this.functionToTest = functionToTest;
+    }
+
+    public FunctionTester(List<T> inputs, List<R> expectedResults, CustomFunction<T, R> customFunction) {
+        this.inputs = inputs;
+        this.expectedResults = expectedResults;
+        this.functionToTest = customFunction::apply;
+    }
+
+    public void testFunction() {
+        for (int i = 0; i < inputs.size(); i++) {
+            T input = inputs.get(i);
+            R expectedResult = expectedResults.get(i);
+            R actualResult = functionToTest.apply(input);
+
+            if (actualResult.equals(expectedResult)) {
+                System.out.println("Test passed for input " + input + ". Expected result: " + expectedResult + ", Actual result: " + actualResult);
+            } else {
+                System.out.println("Test failed for input " + input + ". Expected result: " + expectedResult + ", Actual result: " + actualResult);
+            }
+        }
+    }
+}
+
+@FunctionalInterface
+interface CustomFunction<T, R> {
+    R apply(T input);
+}
